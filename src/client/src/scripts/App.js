ad22e01759b9be9dce7089fa10272c81ece97be0
Hugo Piévic <hugo.pievic@axa.fr>
2017-07-10 11:43:38 +0200

    init client with create-react-app

---
+++ src/client/src/scripts/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../assets/logo.svg';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Welcome
+      </div>
+    );
+  }
+}
+
+export default App;
