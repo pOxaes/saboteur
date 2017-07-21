2f75643c9d9f0900705ecd9e22256b46d3aecda3
Hugo Piévic <hugo.pievic@axa.fr>
2017-07-22 00:43:18 +0200

    players position WIP

--- src/client/src/scripts/components/PlayersList.js
+++ src/client/src/scripts/components/PlayersList.js
@@ -5,6 +5,50 @@ import "../../styles/PlayersList.css";
 
 const RESIZE_THROTTLE_TIME = 300;
 
+const drawCircle = (radius, x, y) => {
+  const circle = document.createElement('div');
+  circle.style.position = "absolute";
+  circle.style.left = x + "px";
+  circle.style.top = y + "px";
+  circle.style.marginLeft = -radius + "px";
+  circle.style.marginTop = -radius + "px";
+  circle.style.width = 2 * radius - 4 + "px";
+  circle.style.height = 2 * radius - 4 + "px";
+  circle.style.pointerEvents = "none";
+  circle.style.borderRadius = "50%";
+  circle.style.border = "2px solid rgba(255, 0, 0, .2)";
+  circle.style.boxSizing = "content-box";
+  document.body.appendChild(circle);
+}
+
+const drawLine = (radius, rotateVal, x, y, circleX, circleY) => {
+  const line = document.createElement("div");
+  line.style.border = "1px solid rgba(255, 0, 0, 0.2)";
+  line.style.width = 2 * radius + "px";
+  line.style.height = "0px";
+  line.style.pointerEvents = "none";
+  line.style.position = "absolute";
+  line.style.top = circleY + "px";
+  line.style.left = circleX + "px";
+  line.style.marginLeft = -radius + "px";
+  line.style.transform = `rotate(${rotateVal}deg)`;
+
+  const dot = document.createElement("div");
+  dot.style.background = "green";
+  dot.style.position = "absolute";
+  dot.style.width = "10px";
+  dot.style.height = "10px";
+  dot.style.top = y + "px";
+  dot.style.pointerEvents = "none";
+  dot.style.left = x + "px";
+  dot.style.marginLeft = "-5px";
+  dot.style.marginTop = "-5px";
+  dot.style.borderRadius = "50%";
+
+  document.body.appendChild(line);
+  document.body.appendChild(dot);
+}
+
 const degToRad = deg => deg * Math.PI / 180;
 
 const getDirectionFromAngle = angle => {
@@ -49,25 +93,39 @@ export default class PlayersList extends Component {
   }
 
   updatePositions() {
+    const maxAngle = 210;
     const positions = [];
 
     const bodyClientRect = document.body.getBoundingClientRect();
-    const circleXPadding = -20;
-    const circleYPadding = -20;
+    const circleXPadding = 0;
+    const circleYPadding = 0;
 
-    const circleRadius = (bodyClientRect.width - 2 * circleXPadding) / 2;
+    const circleRadius = Math.min(
+      (bodyClientRect.width - 2 * circleXPadding) / 2,
+      (bodyClientRect.height - 2 * circleYPadding) / 1.5
+    );
+    
     const circleY = circleRadius + circleYPadding;
-    const circleX = circleRadius + circleXPadding;
+    const circleX = bodyClientRect.width / 2;
+    // drawCircle(circleRadius, circleX, circleY);
+    const angle = maxAngle / this.props.players.length;
+    const angleOffset = angle / 2 - (maxAngle - 180) / 2;
 
-    const angle = 180 / this.props.players.length;
-    const angleOffset = angle / 2;
+    for (let rotateVal = angleOffset; rotateVal < maxAngle + angleOffset; rotateVal += angle) {
 
-    for (let rotateVal = angleOffset; rotateVal < 180 + angleOffset; rotateVal += angle) {
+      let xFromCenter = circleRadius * Math.cos(degToRad(rotateVal));
+      let yFromCenter = circleRadius * Math.sin(degToRad(rotateVal));
+
+      let x = circleX - xFromCenter;
+      let y = circleY - yFromCenter;
+
       positions.push({
-        x: circleX - circleRadius * Math.cos(degToRad(rotateVal)),
-        y: circleY - circleRadius * Math.sin(degToRad(rotateVal)),
-        direction: getDirectionFromAngle(rotateVal),
+        x,
+        y,
+        direction: getDirectionFromAngle(rotateVal, x, y),
       });
+
+      // drawLine(circleRadius, rotateVal, x, y, circleX, circleY);
     }
     this.setState({
       positions,
